18e8d91fabd86886b9b920868ef0df8a746c9c45
7underlines <7underlines@gmail.com>
2025-07-05 11:00:33 +0200

    rework

--- src/content.config.mjs
+++ src/content.config.mjs
@@ -5,24 +5,16 @@ import { defineCollection, z } from 'astro:content'
 import { glob } from 'astro/loaders'
 
 // 3. Define your collection(s)
-const projects = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/content/projects' }),
-  schema: z.object({
-    title: z.string(),
-    author: z.string(),
-    description: z.string(),
-  }),
-})
-
-const posts = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/content/posts' }),
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.mdx', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     author: z.string(),
     description: z.string(),
     date: z.string().optional(),
+    image: z.string(),
   }),
 })
 
 // 4. Export a single `collections` object to register you collection(s)
-export const collections = { projects, posts }
+export const collections = { blog }
